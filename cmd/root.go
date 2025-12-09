d10d5154c58dd7aa00ebbd72b6003ae80c6ff847
harryzhu <harry.chuh@gmail.com>
2025-12-09 12:12:57 +0800

    -

--- cmd/root.go
+++ cmd/root.go
@@ -82,26 +82,26 @@ func Execute() {
 
 func init() {
 	rootCmd.PersistentFlags().BoolVar(&IsDebug, "debug", false, "if print debug info")
-	rootCmd.PersistentFlags().BoolVar(&IsWithLimitMemory, "with-limit-memory", false, "")
-	rootCmd.PersistentFlags().BoolVar(&IsWithTimeUTC, "with-time-utc", false, "")
-	rootCmd.PersistentFlags().BoolVar(&IsInSingleHDD, "in-single-hdd", false, "")
+	rootCmd.PersistentFlags().BoolVar(&IsWithLimitMemory, "with-limit-memory", false, "run with low momery, task will be forced to 4 threads")
+	rootCmd.PersistentFlags().BoolVar(&IsWithTimeUTC, "with-time-utc", false, "use UTC timezone with parameter --min-age / --max-age")
+	rootCmd.PersistentFlags().BoolVar(&IsInSingleHDD, "in-single-hdd", false, "only for massive small files copy on a single low speed hard disk")
 	//
-	rootCmd.Flags().BoolVar(&IsIgnoreDotFile, "ignore-dot-file", true, "")
-	rootCmd.Flags().BoolVar(&IsIgnoreEmptyFolder, "ignore-empty-folder", true, "")
-	rootCmd.Flags().BoolVar(&IsOverwrite, "overwrite", false, "")
+	rootCmd.Flags().BoolVar(&IsIgnoreDotFile, "ignore-dot-file", true, "ignore the file if its file name starts with dot(.), i.e.: .DS_Store")
+	rootCmd.Flags().BoolVar(&IsIgnoreEmptyFolder, "ignore-empty-folder", true, "ignore the folder if it contains nothing")
+	rootCmd.Flags().BoolVar(&IsOverwrite, "overwrite", false, "allow to overwrite the existing files")
 	//
-	rootCmd.Flags().StringVar(&SourceDir, "source-dir", "", "")
-	rootCmd.Flags().StringVar(&TargetDir, "target-dir", "", "")
+	rootCmd.Flags().StringVar(&SourceDir, "source-dir", "", "source folder")
+	rootCmd.Flags().StringVar(&TargetDir, "target-dir", "", "destination folder")
 	//
-	rootCmd.Flags().StringVar(&ExcludeDir, "exclude-dir", "", "")
-	rootCmd.Flags().StringVar(&FileExt, "file-ext", "", "")
+	rootCmd.Flags().StringVar(&ExcludeDir, "exclude-dir", "", "will not copy the file if it is in the exclude-dir")
+	rootCmd.Flags().StringVar(&FileExt, "file-ext", "", "file type filter, i.e.: .mp4 or .png or .jpg ... ")
 	//
-	rootCmd.Flags().Int64Var(&MinSize, "min-size", -1, "")
-	rootCmd.Flags().Int64Var(&MaxSize, "max-size", -1, "")
+	rootCmd.Flags().Int64Var(&MinSize, "min-size", -1, "from the minimum file size")
+	rootCmd.Flags().Int64Var(&MaxSize, "max-size", -1, "to the maximum file size")
 	//
 	rootCmd.Flags().StringVar(&MinAge, "min-age", "", "format: 20231203150908, means 2023-12-03 15:09:08")
 	rootCmd.Flags().StringVar(&MaxAge, "max-age", "", "format: 20231225235959, means 2023-12-25 23:59:59")
 	//
-	rootCmd.Flags().IntVar(&ThreadNum, "threads", 0, "")
+	rootCmd.Flags().IntVar(&ThreadNum, "threads", 0, "force the concurrent tasks, more threads, more memory required")
 	//
 }
